77ef19332bd008d9bdfa321b31be93a18c4b93b0
Jules ARTAUD <jules.artaud@outlook.com>
2022-05-23 14:06:39 +0200

    1er commit

---
+++ controleur/menu.php
@@ -0,0 +1,11 @@
+<ul>
+
+    <li><a href="./?action=deconnexion"> DECONNEXION</a></li>
+    <li><a href="../index.php?">ACCEUIL</a></li>
+    <li><a href="index.php?action=modification">MODIFIER UN ARBITRE</a></li>
+    <li><a href="index.php?action=ajout">AJOUTER UN ARBITRE</a></li>
+    <li><a href="index.php?action=info">VOIR LES INFORMATIONS DES MATCHS</a></li>
+    <li><a href="index.php?action=connexion">CONNEXION</a></li>
+
+
+</ul>
