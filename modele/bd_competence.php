d2fa0331403b53b4e4c9cf8b1161ad7dc7d7d955
Delannoy <delannoy.jeremyd@gmail.com>
2022-05-23 15:02:07 +0200

    fonction insert pas test

---
+++ modele/bd_competence.php
@@ -0,0 +1,31 @@
+<?php
+
+include "bd_connexion.php";
+function InsererCompetence($nomCompetence ,$idBloc){
+    try {
+        $connex = connexionPDO();
+        $rec = $connex->prepare("INSERT INTO competence VALUES(:idBloc , null, :nomCompetence)");
+        $rec->bindValue('idBloc', $idBloc);
+        $rec->bindValue('nomCompetence', $nomCompetence);
+        $rec->execute();
+    }catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+}
+
+function InsererSousCompetence($nomSousCompetence ,$idCompetence ,$idBloc){
+    try {
+        $connex = connexionPDO();
+        $rec = $connex->prepare("INSERT INTO sous_Competence VALUES(:idBloc , :idCompetence , null, :nomSousCompetence)");
+        $rec->bindValue('idBloc', $idBloc);
+        $rec->bindValue('idCompetence', $idCompetence);
+        $rec->bindValue('nomSousCompetence', $nomSousCompetence);
+        $rec->execute();
+    }catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+}
+
+?>
