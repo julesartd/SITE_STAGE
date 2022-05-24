6df1e503862427e3a7c8cf6201bace69a5eda405
Delannoy <delannoy.jeremyd@gmail.com>
2022-05-24 09:17:28 +0200

    recup

--- modele/bd_competence.php
+++ modele/bd_competence.php
@@ -51,7 +51,7 @@ return $resultat;
 function getBloc() { 
     try {
         $connex = connexionPDO();
-        $rec = $connex->prepare("SELECT * FROM bloc");
+        $rec = $connex->prepare("select * from bloc");
         $rec->execute();
     }catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
