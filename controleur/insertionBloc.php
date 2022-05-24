2869916155484458ba8a3b078e1a53fa0272af1b
Jules Artaud <jules.artaud@outlook.com>
2022-05-24 09:21:37 +0200

    insertion competence

--- controleur/insertionBloc.php
+++ controleur/insertionBloc.php
@@ -3,27 +3,19 @@
 
     include "modele/bd_competence.php";
 
-  
-
-    // if (empty($_SESSION['mailU']) || empty($_SESSION['mdpU'])) {
-    //     header("Location:/?action=");
-    // }
+    
 
     if (isset($_POST['btnAjout'])) {
         if (isset($_POST['txtIDBLOC'], $_POST['txtNomBloc'], $_POST['txtIDBAC'])) {
 
-            insertCompetence($_POST['txtIDBLOC'], $_POST['txtNomBloc'], $_POST['txtIDBAC']);
+            insertCompetenceChapeau($_POST['txtIDBLOC'], $_POST['txtNomBloc'], $_POST['txtIDBAC']);
         } 
     }
     $listeBac = getBac();
-    print_r($listeBac);
-
-    echo($_POST['txtIDBLOC']);
-    echo($_POST['txtNomBloc']);
-    echo($_POST['txtIDBAC']);
+   
   
+    include "vue/vueBloc.php";
     
-    
-    include "vue/vueBloc.php"
+
     ?>
 </div>
