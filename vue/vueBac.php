69cd8c6ced488fa61f5a3795837ad385633bdaa0
Jules Artaud <jules.artaud@outlook.com>
2022-05-25 10:30:06 +0200

    commit 3 jour

--- vue/vueBac.php
+++ vue/vueBac.php
@@ -1,9 +1,9 @@
-<form method="POST" class="lb mb-3">
+<form method="POST" class="lb mb-3" >
     <h1 id="lstA">Ajouter un Baccalauréat</h1>
     </br>
     <caption>Insertion d'un Bac :</caption>
-    <input type="text" name='txtNomBac' placeholder='Nom du bac' required>
-    <input type="submit" value="AJOUTER" name="btnAjout">
+    <input type="text" name='txtNomBac' placeholder='Nom du bac' required class="form-label">
+    <input type="submit" value="AJOUTER" name="btnAjout" >
     </br>
     </br>
     <h2>Voici la liste des Bacs déjà enregistrés</h2>
