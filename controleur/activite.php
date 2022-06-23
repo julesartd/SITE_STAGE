062ff9027ba69cc9dab1ec2df5bfb97d2039397d
Jules Artaud <jules.artaud@outlook.com>
2022-06-23 13:26:59 +0200

    securite

--- controleur/activite.php
+++ controleur/activite.php
@@ -7,33 +7,32 @@
     include_once "modele/bd_classe.php";
     include_once "modele/bd_competence.php";
 
+    if (isset($_SESSION["mailUtilisateur"])) {
+        if ($_SESSION["idDroitUtilisateur"] == 1) {
 
-    if ($_SESSION["idDroitUtilisateur"] == 1) {
+            $listeDiplome = getDiplome();
+            $listeClasse = getClasse();
+            $listeProf = getProf();
+            if (isset($_POST['btnAjoutActivite'])) {
+                print_r($_POST['SelectCompetence']);
+                insertActivite($_POST['txtNomActivite'], $_POST['selectCompetence'], $_POST['professeur'], $_POST['selectClasse']);
 
-        $listeDiplome = getDiplome();
-        $listeClasse = getClasse();
-        $listeProf = getProf();
-        if (isset($_POST['btnAjoutActivite'])) {
-            print_r( $_POST['SelectCompetence']);
-            insertActivite($_POST['txtNomActivite'], $_POST['selectCompetence'], $_POST['professeur'], $_POST['selectClasse']);
-
-            header("Location:index.php?action=activite");
+                header("Location:index.php?action=activite");
+            }
+            include "vue/vueActivite.Admin.php";
         }
-        include "vue/vueActivite.Admin.php";
-    }else {
-        header("Location:/?action=connexion&login=non");
-    }
-    if ($_SESSION["idDroitUtilisateur"] == 2) {
-        
-        $listeDiplome = getDiplome();
-        $listeClasse = getClasseByIdProf($_SESSION["idProfesseur"]);
-        if (isset($_POST['btnAjoutActivite'])) {
-            insertActivite($_POST['txtNomActivite'], $_POST['SelectCompetence'], $_SESSION['idProfesseur'], $_POST['selectClasse']);
+        if ($_SESSION["idDroitUtilisateur"] == 2) {
 
-            header("Location:index.php?action=activite");
+            $listeDiplome = getDiplome();
+            $listeClasse = getClasseByIdProf($_SESSION["idProfesseur"]);
+            if (isset($_POST['btnAjoutActivite'])) {
+                insertActivite($_POST['txtNomActivite'], $_POST['SelectCompetence'], $_SESSION['idProfesseur'], $_POST['selectClasse']);
+
+                header("Location:index.php?action=activite");
+            }
+            include "vue/vueActivite.Prof.php";
         }
-        include "vue/vueActivite.Prof.php";
-    }else {
+    } else {
         header("Location:/?action=connexion&login=non");
     }
     ?>
--- vue/vueAuthentification.php
+++ vue/vueAuthentification.php
@@ -1,9 +1,13 @@
-<h1>Connexion</h1>
-<form action="./?action=connexion" method="POST">
- 
-    <input required type="text" name="mailU" placeholder="Nom d'utilisateur" /><br />
-    <input required type="password" name="mdpU" placeholder="Mot de passe"  /><br />
-    <input type="submit" />
- 
-</form>
-<br />
+<div class="mb-3 connexion">
+    <h1>Connexion</h1>
+
+    </br>
+
+    <form action="./?action=connexion" method="POST">
+
+        <input required class="form-control" type="text" name="mailU" placeholder="Nom d'utilisateur" /><br />
+        <input required class="form-control" type="password" name="mdpU" placeholder="Mot de passe" /><br />
+        <input type="submit"  class="btn btn-primary" value="Se connecter"/>
+
+    </form>
+</div>
